fcf4cb2c41b7c3361d462401e906d065960e4db4
Jonathan Neal <jonathantneal@hotmail.com>
2012-09-19 09:21:24 -0700

    Readability improvements, Demo
    No functionality is changed in this update. All of the changes are
    there to improve readability. A special thanks to cowboy (Ben Alman).

--- EventListener.js
+++ EventListener.js
@@ -1,29 +1,40 @@
 // EventListener | @jon_neal | MIT/GPL2
-!window.addEventListener && Element.prototype && (function (WindowPrototype, DocumentPrototype, ElementPrototype, addEventListener, removeEventListener, dispatchEvent, registry) {
-	WindowPrototype[addEventListener] = DocumentPrototype[addEventListener] = ElementPrototype[addEventListener] = function (type, listener) {
+!window.addEventListener && Element.prototype && (function () {
+	function addToPrototype(name, method) {
+		Window.prototype[name] = HTMLDocument.prototype[name] = Element.prototype[name] = method;
+	}
+
+	var registry = [];
+
+	addToPrototype("addEventListener", function (type, listener) {
 		var target = this;
 
-		registry.unshift([target, type, listener, function (event) {
-			event.currentTarget = target;
-			event.preventDefault = function () { event.returnValue = false };
-			event.stopPropagation = function () { event.cancelBubble = true };
-			event.target = event.srcElement || target;
+		registry.unshift({
+			__listener: function (event) {
+				event.currentTarget = target;
+				event.preventDefault = function () { event.returnValue = false };
+				event.stopPropagation = function () { event.cancelBubble = true };
+				event.target = event.srcElement || target;
 
-			listener.call(target, event);
-		}]);
+				listener.call(target, event);
+			},
+			listener: listener,
+			target: target,
+			type: type
+		});
 
-		this.attachEvent("on" + type, registry[0][3]);
-	};
+		this.attachEvent("on" + type, registry[0].__listener);
+	});
 
-	WindowPrototype[removeEventListener] = DocumentPrototype[removeEventListener] = ElementPrototype[removeEventListener] = function (type, listener) {
-		for (var index = 0, register; register = registry[index]; ++index) {
-			if (register[0] == this && register[1] == type && register[2] == listener) {
-				return this.detachEvent("on" + type, registry.splice(index, 1)[0][3]);
+	addToPrototype("removeEventListener", function (type, listener) {
+		for (var index = 0; registry[index]; ++index) {
+			if (registry[index].target == this && registry[index].type == type && registry[index].listener == listener) {
+				return this.detachEvent("on" + type, registry.splice(index, 1)[0].__listener);
 			}
 		}
-	};
+	});
 
-	WindowPrototype[dispatchEvent] = DocumentPrototype[dispatchEvent] = ElementPrototype[dispatchEvent] = function (eventObject) {
+	addToPrototype("dispatchEvent", function (eventObject) {
 		return this.fireEvent("on" + eventObject.type, eventObject);
-	};
-})(Window.prototype, HTMLDocument.prototype, Element.prototype, "addEventListener", "removeEventListener", "dispatchEvent", []);
+	});
+})();
